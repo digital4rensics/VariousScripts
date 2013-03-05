809a860eeb7725459742512c856d2f16d9356347
digital4rensics <keith@digital4rensics.com>
2013-03-04 21:05:17 -0500

    Added option parsing & minimum threshold

--- PairSort.py
+++ PairSort.py
@@ -1,31 +1,40 @@
 #!/usr/bin/env python
 
 """
+PairSort.py originally written by Keith Gilbert - @digital4rensics
+www.digital4rensics.com - 3/4/12 - Version 0.2
+
 This script will parse comma separated elements in a file (2 per line) and produce
 a full list of matches for each element. Example output is below.
 
 No warranty is implied or expressed, use at your own risk.
 
-USAGE: python PairSort.py <INPUT FILE> <OUTPUT FILE>
+usage: PairSort.py [-h] [-c COUNT] source destination
 
-Input:
-	a,b
-	b,c
-	b,a
-	c,d
-Output:
-	a: b
-	b: c,a
-	c: b,d
-	d: c
+Parse comma separated pairs and output unique matches by element
+
+positional arguments:
+  source                Specify the source data file
+  destination           Specify the destination data file
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -c COUNT, --count COUNT
+                        Set the threshold for minimum number of matches to be
+                        included in the output
 """
 
 import sys
+import argparse
 
-target = sys.argv[1]
-dest = sys.argv[2]
+parser = argparse.ArgumentParser(description="Parse comma separated pairs and output unique matches by element")
+parser.add_argument("source", help="Specify the source data file")
+parser.add_argument("destination", help="Specify the destination data file")
+parser.add_argument("-c", "--count", type=int, help="Set the threshold for minimum number of matches to be included in the output")
 
-data = open(target, 'r')
+args = parser.parse_args()
+
+data = open(args.source, 'r')
 
 pairs = {}
 
@@ -49,12 +58,19 @@ for line in data:
 
 data.close()
 
-out = open(dest, 'w')
-
-for key in pairs:
-	out.write(key + ":\n")
-	for entry in pairs[key]:
-		out.write("\t" + entry + "\n")
-	out.write("\n")
+out = open(args.destination, 'w')
+if args.count:
+	for key in pairs:
+		if len(pairs[key]) >= args.count:
+			out.write(key + ":\n")
+			for entry in pairs[key]:
+				out.write("\t" + entry + "\n")
+			out.write("\n")
+else:
+	for key in pairs:
+		out.write(key + ":\n")
+		for entry in pairs[key]:
+			out.write("\t" + entry + "\n")
+		out.write("\n")
 
 out.close()
